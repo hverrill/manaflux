4c60cde4497ad0f83c982c51906c7f0700d216d4
FourInchKnife <riley.j.mclain@gmail.com>
2021-05-19 15:56:59 -0400

    changed the item group

--- src/main/java/io/github/hverrill/manaflux/Manaflux.java
+++ src/main/java/io/github/hverrill/manaflux/Manaflux.java
@@ -1,12 +1,18 @@
 package io.github.hverrill.manaflux;
 
+import io.github.hverrill.manaflux.registry.ManasteelIngotsUtils.ManasteelIngots;
 import io.github.hverrill.manaflux.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class Manaflux implements ModInitializer {
-	public static final ItemGroup ITEMGROUP = ItemGroup.MISC;
 	public static final String MOD_ID = "manaflux";
+	public static final ItemGroup ITEMGROUP = FabricItemGroupBuilder.build(
+			new Identifier(MOD_ID, "general"),
+			() -> new ItemStack(ManasteelIngots.MANASTEEL_INGOT));
 
 	@Override
 	public void onInitialize() {
