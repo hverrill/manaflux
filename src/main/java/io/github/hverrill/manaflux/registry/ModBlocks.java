eaff39ebed6004dc5022bed8eb1fc8aa7f641d16
hverrill <52667642+hverrill@users.noreply.github.com>
2021-05-19 21:48:31 -0400

    added sequence workbench hopefully

--- src/main/java/io/github/hverrill/manaflux/registry/ModBlocks.java
+++ src/main/java/io/github/hverrill/manaflux/registry/ModBlocks.java
@@ -1,7 +1,23 @@
 package io.github.hverrill.manaflux.registry;
 
+import io.github.hverrill.manaflux.Manaflux;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ModBlocks {
-    public static void registerItems(){
-        
+    public static final Block SEQUENCE_WORKBENCH = new Block(FabricBlockSettings
+        /* Block Settings: */
+        .of(Material.WOOD)
+        .breakByHand(true)
+        .strength(1.0f, 1.0f)
+        .sounds(BlockSoundGroup.WOOD)
+        .nonOpaque()
+        );
+    public static void registerBlocks(){
+        Registry.register(Registry.BLOCK, new Identifier(Manaflux.MOD_ID, "sequence_workbench"), SEQUENCE_WORKBENCH);
     }
 }
