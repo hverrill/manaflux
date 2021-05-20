47d5f1b1b4f25d03413545177fc8017c47e52dc5
hverrill <52667642+hverrill@users.noreply.github.com>
2021-05-20 17:39:52 -0400

    added composite blocks

--- src/main/java/io/github/hverrill/manaflux/registry/ModBlocks.java
+++ src/main/java/io/github/hverrill/manaflux/registry/ModBlocks.java
@@ -1,9 +1,11 @@
 package io.github.hverrill.manaflux.registry;
 
+import io.github.hverrill.manaflux.blocks.CompositeBlocks;
 import io.github.hverrill.manaflux.blocks.SequenceWorkbench;
 
 public class ModBlocks {
     public static void registerBlocks(){
         SequenceWorkbench.init();
+        CompositeBlocks.init();
     }
 }
