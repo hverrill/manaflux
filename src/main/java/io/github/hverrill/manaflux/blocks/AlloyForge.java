3d9026bb3d853904163f3f834dea246484a6d80f
FourInchKnife <riley.j.mclain@gmail.com>
2021-05-21 17:11:07 -0400

    made the Alloy Forge work like a furnace for the time being

--- src/main/java/io/github/hverrill/manaflux/blocks/AlloyForge.java
+++ src/main/java/io/github/hverrill/manaflux/blocks/AlloyForge.java
@@ -5,19 +5,24 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.*;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.FurnaceBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.Properties;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
@@ -46,25 +51,44 @@ public class AlloyForge{
         );
     }
 
-    public static class AlloyForgeBlock extends HorizontalFacingBlock {
-        public AlloyForgeBlock(Settings settings) {
+    public static class AlloyForgeBlock extends AbstractAlloyForge{
+        protected AlloyForgeBlock(AbstractBlock.Settings settings) {
             super(settings);
-            setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
         }
 
-        @Override
-        protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
-            stateManager.add(Properties.HORIZONTAL_FACING);
+        public BlockEntity createBlockEntity(BlockView world) {
+            return new FurnaceBlockEntity();
         }
 
-        @Override
-        @SuppressWarnings("deprecation")
-        public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-            // Direction dir = state.get(FACING);
-            return VoxelShapes.fullCube();
+        protected void openScreen(World world, BlockPos pos, PlayerEntity player) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof FurnaceBlockEntity) {
+                player.openHandledScreen((NamedScreenHandlerFactory)blockEntity);
+                player.incrementStat(Stats.INTERACT_WITH_FURNACE);
+            }
+
         }
-        public BlockState getPlacementState(ItemPlacementContext ctx) {
-            return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+
+        @Environment(EnvType.CLIENT)
+        public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+            if (state.get(LIT)) {
+                double d = (double)pos.getX() + 0.5D;
+                double e = pos.getY();
+                double f = (double)pos.getZ() + 0.5D;
+                if (random.nextDouble() < 0.1D) {
+                    world.playSound(d, e, f, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+                }
+
+                Direction direction = state.get(FACING);
+                Direction.Axis axis = direction.getAxis();
+                //double g = 0.52D;
+                double h = random.nextDouble() * 0.6D - 0.3D;
+                double i = axis == Direction.Axis.X ? (double)direction.getOffsetX() * 0.52D : h;
+                double j = random.nextDouble() * 6.0D / 16.0D;
+                double k = axis == Direction.Axis.Z ? (double)direction.getOffsetZ() * 0.52D : h;
+                world.addParticle(ParticleTypes.SMOKE, d + i, e + j, f + k, 0.0D, 0.0D, 0.0D);
+                world.addParticle(ParticleTypes.FLAME, d + i, e + j, f + k, 0.0D, 0.0D, 0.0D);
+            }
         }
     }
 }
